ba01e73f10953dd9f708f8f308abb30407cf450b
starkoka <j22437@inc.kisarazu.ac.jp>
2024-03-10 14:06:13 +0900

    feat:src/converter/segments.hにhttplibのincludeを追加

--- src/converter/segments.h
+++ src/converter/segments.h
@@ -48,6 +48,7 @@
 #include "base/strings/assign.h"
 #include "converter/lattice.h"
 #include "testing/friend_test.h"
+#include "third_party/cpp-httplib/httplib.h"
 
 #ifndef NDEBUG
 #define MOZC_CANDIDATE_DEBUG
