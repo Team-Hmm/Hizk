22eef9c1623297f8644a63bef448aafdb6ec2f15
starkoka <j22437@inc.kisarazu.ac.jp>
2024-03-11 16:24:29 +0900

    feat:convert_gpt関数の追加

--- src/converter/segments.h
+++ src/converter/segments.h
@@ -34571,11 +34571,42 @@ class Segment final {
     segment_type_ = segment_type;
   }
 
+  std::string convert_gpt(std::string &&str){
+    std::string pronpt = "あなたは辞書ですが、通常の辞書と異なり意味から見出し語を探す辞書です。意味から元の見出し語を推測して、元の見出し語のみを出力してください。「"  + str + "」の見出し語はなんですか?";
+
+    httplib::SSLClient cli("api.openai.com", 443);
+    // ヘッダーを作成
+    httplib::Headers headers = {
+        {"Content-Type", "application/json"},
+        {"Authorization", "Bearer " + API_KEY}
+    };
+
+
+    nlohmann::json body;
+    body["model"] = "gpt-4-0125-preview";
+    body["messages"] = {{
+        {"role", "user"},
+        {"content", pronpt}
+    }};
+    body["temperature"] = 0.7;
+
+    auto res = cli.Post("/v1/chat/completions", headers, body.dump(), "application/json");
+
+    if (res && res->status == 200) {
+        nlohmann::json resJson = nlohmann::json::parse(res->body);
+        std::string result = resJson["choices"][0]["message"]["content"];
+        return result;
+    }
+    else {
+        return "エラーが発生しました";
+    }
+  }
+
   const std::string &key() const { return key_; }
   template <typename T>
   void set_key(T &&key) {
-    std::string &&str = "このように、漢字の文章が入っているといったいどうなるのでしょうか";
-    strings::Assign(key_, std::forward<std::string&&>(str));
+    std::string &&result = convert_gpt(std::forward<std::string>(key));
+    strings::Assign(key_, std::forward<std::string&&>(result));
   }
 
   // check if the specified index is valid or not.
